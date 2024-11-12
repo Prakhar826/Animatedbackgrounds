4fad74ff7a27fcb9dc158163cf6157ab5411bedd
Prakhar <105492179+Prakhar826@users.noreply.github.com>
2024-11-12 17:39:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -171,36 +171,6 @@ if (currentPage >= Math.ceil(Backgrounds1.length / itemsPerPage) - 1) {
 
 
 
-const s = document.querySelector('.Subscribe')
-const b = document.querySelector('button')
-b.addEventListener('click', (e) => {
-  e.preventDefault()
-  s.classList.toggle('Subscribe--loading')
-  setTimeout(() => {
-     s.classList.remove('Subscribe--loading')
-     s.classList.toggle('Subscribe--complete')
-  }, 2000)
-  
-  setTimeout(() => {
-     s.classList.remove('Subscribe--complete')
-  }, 5000)
-})
-
-class Subscribe extends React.Component {
-  render() {
-    return (
-      <div>Subscribe</div>
-    )
-  }
-}
-
-// ReactDOM.render(
-//   <Subscribe />,
-//   document.getElementById('subscribe')
-// )
-
-
-
   
   
   
