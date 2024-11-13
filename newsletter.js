cc90ea825da4999da6feb761d398c42c8bd5108a
Prakhar <105492179+Prakhar826@users.noreply.github.com>
2024-11-13 11:49:22 +0530

    Update newsletter.js

--- newsletter.js
+++ newsletter.js
@@ -1,26 +1,25 @@
+const s = document.querySelector('.Subscribe');
+const b = document.querySelector('button');
+b.addEventListener('click', e => {
+  e.preventDefault();
+  s.classList.toggle('Subscribe--loading');
+  setTimeout(() => {
+    s.classList.remove('Subscribe--loading');
+    s.classList.toggle('Subscribe--complete');
+  }, 2000);
 
-const s = document.querySelector('.Subscribe')
-const b = document.querySelector('button')
-b.addEventListener('click', (e) => {
-  e.preventDefault()
-  s.classList.toggle('Subscribe--loading')
   setTimeout(() => {
-     s.classList.remove('Subscribe--loading')
-     s.classList.toggle('Subscribe--complete')
-  }, 2000)
-  
-  setTimeout(() => {
-     s.classList.remove('Subscribe--complete')
-  }, 5000)
-})
+    s.classList.remove('Subscribe--complete');
+  }, 5000);
+});
 
 class Subscribe extends React.Component {
   render() {
-    return (
-      <div>Subscribe</div>
-    )
-  }
-}
+    return /*#__PURE__*/(
+      React.createElement("div", null, "Subscribe"));
+
+  }}
+
 
 // ReactDOM.render(
 //   <Subscribe />,
