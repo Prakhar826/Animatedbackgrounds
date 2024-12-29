b4aded5242a60991ceec521380e7a2326ff721f8
Prakhar <105492179+Prakhar826@users.noreply.github.com>
2024-12-29 06:22:14 +0530

    Update newsletter.js

--- newsletter.js
+++ newsletter.js
@@ -1,36 +1,3 @@
-const s = document.querySelector('.Subscribe');
-const b = document.querySelector('button');
-const emailInput = document.querySelector('input[type="text"]');
-const errorMessage = document.querySelector('.error-message');
-
-b.addEventListener('click', (e) => {
-  e.preventDefault();
-  const emailValue = emailInput.value.trim();
-
-  if (emailValue !== '') {
-    // Hide the error message
-    errorMessage.style.display = 'none';
-
-    // Show loading state
-    s.classList.toggle('Subscribe--loading');
-    setTimeout(() => {
-      s.classList.remove('Subscribe--loading');
-      s.classList.toggle('Subscribe--complete');
-    }, 2000);
-
-    // Remove complete state
-    setTimeout(() => {
-      s.classList.remove('Subscribe--complete');
-      // Clear the input field
-      emailInput.value = '';
-    }, 5000);
-  } else {
-    // Show the error message
-    errorMessage.textContent = 'Please enter a valid email address.';
-    errorMessage.style.display = 'block';
-  }
-});
-
 
 
 
@@ -75,3 +42,49 @@ var sender = function () {
     document.getElementById("message").value = "";
     return false;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+const s = document.querySelector('.Subscribe');
+const b = document.querySelector('button');
+const emailInput = document.querySelector('input[type="text"]');
+const errorMessage = document.querySelector('.error-message');
+
+b.addEventListener('click', (e) => {
+  e.preventDefault();
+  const emailValue = emailInput.value.trim();
+
+  if (emailValue !== '') {
+    // Hide the error message
+    errorMessage.style.display = 'none';
+
+    // Show loading state
+    s.classList.toggle('Subscribe--loading');
+    setTimeout(() => {
+      s.classList.remove('Subscribe--loading');
+      s.classList.toggle('Subscribe--complete');
+    }, 2000);
+
+    // Remove complete state
+    setTimeout(() => {
+      s.classList.remove('Subscribe--complete');
+      // Clear the input field
+      emailInput.value = '';
+    }, 5000);
+  } else {
+    // Show the error message
+    errorMessage.textContent = 'Please enter a valid email address.';
+    errorMessage.style.display = 'block';
+  }
+});
+
+
