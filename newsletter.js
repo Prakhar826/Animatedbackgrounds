b3dadfd17563855b4da43d07ad4d22dfc06c0919
Prakhar <105492179+Prakhar826@users.noreply.github.com>
2025-01-03 18:31:45 +0530

    Update newsletter.js

--- newsletter.js
+++ newsletter.js
@@ -9,7 +9,7 @@ var ready = function () {
 const s = document.querySelector('.Subscribe');
 
 var sender = function () {
-    event.preventDefault();
+    
     ready();
     var settings = {
         "async": true,
