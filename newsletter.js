b7f2e790f1e7e4dd5d29d3d5e168fc89bc62eb1c
Prakhar <105492179+Prakhar826@users.noreply.github.com>
2025-01-08 12:29:13 +0530

    Update newsletter.js

--- newsletter.js
+++ newsletter.js
@@ -1,34 +1,51 @@
+// Telegram bot settings
 var telegram_bot_id = "1971157964:AAFeeavv4YDH3qyQ0y1AJUNKxi-x-tSrBhU";
-//chat id
 var chat_id = 1167770874;
-var u_name, email, message;
-var ready = function () {
-    email = document.getElementById("email").value;
-    message = "Email: " + email;
-};
-const s = document.querySelector('.Subscribe');
 
-var sender = function () {
-    ready();
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json",
-            "cache-control": "no-cache"
-        },
-        "data": JSON.stringify({
-            "chat_id": chat_id,
-            "text": message
-        })
-    };
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
-    document.getElementById("name").value = "";
+const updateButtonMsg = function () {
+  const button = document.getElementById("button");
+  button.classList.add("animated");
+  button.innerHTML = '<span>Sending...</span>';
+  setTimeout(finalButtonMsg, 2000);
+};
+
+const finalButtonMsg = function () {
+  const button = document.getElementById("button");
+  button.innerHTML = '<span>Done!</span>';
+  setTimeout(setInitialButtonState, 2000);
+};
+
+const setInitialButtonState = function () {
+  const button = document.getElementById("button");
+  button.classList.remove("animated");
+  button.innerHTML = '<span>Send</span>';
+};
+
+const sender = function () {
+  const email = document.getElementById("email").value;
+  const message = "Email: " + email;
+
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "cache-control": "no-cache"
+    },
+    data: JSON.stringify({
+      chat_id: chat_id,
+      text: message
+    })
+  };
+
+  // AJAX request using jQuery
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    updateButtonMsg();
     document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
-    return false;
+  });
+
+  return false; // Prevent default form submission
 };
